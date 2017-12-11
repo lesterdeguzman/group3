8e3bf3a1a3974923824c6c3538976b6128e2f967
lesterdeguzman <2168659@slu.edu.ph>
2017-12-12 00:08:36 +0800

    lester commit

--- Solutions.java
+++ Solutions.java
@@ -579,3 +579,63 @@ public boolean isValid(String braces) {
         	int x = (n % 2 == 1) ? n / 2 : (n / 2 % 2 == 1) ? n / 2 - 2 : n / 2 - 1;
         	System.out.println(x + " " + (n - x));
     	}
+
+//////////////////////////////////////////////////////////////////Lester
+    	public void Chores() {
+
+			Scanner s = new Scanner(System.in);
+
+			int n = s.nextInt(), k = s.nextInt(), x = s.nextInt();
+			int r = x * k;
+			for (int i = 0; i < n - k; i++) {
+				r += s.nextInt();
+    		}
+
+    		System.out.println(r);
+
+  		}
+public void NikitaString() {
+
+			Scanner sc = new Scanner(System.in);
+
+			while(sc.hasNext())
+			{
+			String in = sc.nextLine();
+			int part1 = 0;
+			int part2 = 0;
+			int part3 = 0;
+			for(int i = 0;i < in.length();i ++){
+				if(in.charAt(i) == 'a'){
+					part1 ++;part3 ++;
+				}
+				if(in.charAt(i) == 'b'){
+					part2 ++;
+				}
+				part2 = Math.max(part1,part2);
+				part3 = Math.max(part3,part2);
+				//System.out.print(part1 + " ");
+				//System.out.print(part2 + " ");
+				//System.out.println(part3);
+			}
+
+			System.out.println(part3);
+
+			}
+		}
+				public void TripForMeal() {
+
+		Scanner sc = new Scanner(System.in);
+
+		int num = sc.nextInt();
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int dist = 0;
+		num--;
+
+		if (num-- > 0) dist += Math.min(a, b);
+		if (dist > 0) dist += num * Math.min(Math.min(a, b), c);
+
+		System.out.println(dist);
+
+		}
